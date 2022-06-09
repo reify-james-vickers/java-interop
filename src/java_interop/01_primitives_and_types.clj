92f840300d872d6a1d64af06c50700c22d0b45b7
reify-james-vickers <james.vickers@reifyhealth.com>
2022-06-09 16:28:21 -0600

    save

--- src/java_interop/01_primitives_and_types.clj
+++ src/java_interop/01_primitives_and_types.clj
@@ -11,6 +11,11 @@
   (type 5) ; java.lang.long
   (type 0x123) ; java.lang.Long (hex)
   (type 0123) ; java.lang.Long (octal)
+  
+  ; note: can use any base in [2, 36]
+  (type 2r101010) ; java.lang.Long (binary)
+  (type 8r123) ; java.lang.Long (octal)
+  
   (= Long (type 5))
   (= Long (type 1.5))
   (type (byte 5)) ; java.lang.Byte
@@ -22,6 +27,9 @@
   (type "foo") ; java.lang.String
   (type false) ; java.lang.Boolean
   (type \a) ; java.lang.Character
+  (type \space) ; java.lang.Character
+  (type \u1234) ; java.lang.Character (unicode)
+  (type \o123) ; java.lang.Character (octal)
   (type {}) ; clojure.lang.PersistentArrayMap
   (type []) ; clojure.lang.PersistentVector 
   (type #{}) ; clojure.lang.PersistentHashSet
@@ -41,3 +49,45 @@
   (supers String)
   (supers (class "foo"))
   (ancestors clojure.lang.PersistentVector))
+
+; math things to know
+(comment
+  (mod 5 2) ; 5 % 2
+  (mod 11 3) ; 11 % 3
+  (quot 5 3) ; 5 / 3
+  (quot 10 3) ; 10 / 3
+
+  (/ 5 3) ; a ratio 5/3, because args are ints and not divisible
+  (def r (/ 5 3))
+  (type r)
+  (numerator r)
+  (denominator r)
+  (.floatValue r)
+  (/ 3 3) ; => 1
+  (/ 5.0 3.0) ; this returns a double
+
+  ; there are literals for special values like Nan and Infinity
+  (Double/isNaN Double/NaN)
+  (Double/isNaN ##NaN)
+  (Double/isInfinite ##Inf)
+  (Double/isInfinite ##-Inf)
+
+  ; clojure throws on overflow/underflow with the commonly used math functions.
+
+  ; throws because the result overflows what long can hold.
+  ; the usual math operations throw on over or underflow.
+  (+ 1 Long/MAX_VALUE)
+  ; arbitrary precision, does not overflow, promotes to larger types when needed.
+  ; there are similar functions like *', -'
+  (+' 1 Long/MAX_VALUE)
+  ; there are also the unchecked-* functions which do normal java math with overflow/underflow.
+  (inc Long/MAX_VALUE) ; throws, overflow
+  (unchecked-inc Long/MAX_VALUE) ; allows overflow
+  (dec Long/MIN_VALUE) ; throws, overflow
+  (unchecked-dec Long/MIN_VALUE) ; allows overflow
+  (unchecked-add 5 Long/MAX_VALUE)
+  (unchecked-multiply 2 Long/MAX_VALUE))
+  
+  
+  
+  
